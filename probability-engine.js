2290fde3e47249a5e04af004b22f2a08a0532cae
Elynnn-wc <elynnn598@gmail.com>
2026-04-19 03:26:47 +0800

    Update probability-engine.js

--- probability-engine.js
+++ probability-engine.js
@@ -2,7 +2,6 @@
  * VIP Fortune Wheel - Probability Engine
  */
 (function() {
-    // Perfectly encoded probabilities and emojis
     const _c = "W3sibmFtZSI6IkpBQ0tQT1QiLCJpY29uIjoi8J+SjiIsIndlaWdodCI6MX0seyJuYW1lIjoiJDUwIENBU0giLCJpY29uIjoi8J+SuCIsIndlaWdodCI6NH0seyJuYW1lIjoiJDEwIFYiLCJpY29uIjoi8J+SpSIsIndlaWdodCI6MTB9LHsibmFtZSI6IiQ1IEMiLCJpY29uIjoi8J+SuCIsIndlaWdodCI6MjV9LHsibmFtZSI6IiQxIFIiLCJpY29uIjoi8J+SsiIsIndlaWdodCI6MzB9LHsibmFtZSI6IlRSWSBBR0FJTiIsImljb24iOiLwn6W6Iiwid2VpZ2h0IjozMH1d";
     let _prizes = [];
     
@@ -14,7 +13,10 @@
     }
     
     window.InternalEngine = {
-        getPrizes: function() { return _prizes; },
+        getPrizes: function() { 
+            // Re-mapped to support our front-end drawing logic!
+            return _prizes.map(p => ({ n: p.name, i: p.icon })); 
+        },
         determineResult: function() {
             const totalWeight = _prizes.reduce((sum, p) => sum + p.weight, 0);
             let random = Math.random() * totalWeight;
